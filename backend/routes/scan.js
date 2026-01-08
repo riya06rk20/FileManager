f3db32fde9921a8cf186e76dc64f5e6a97188aca
riya06rk20 <riya06rk20@gmail.com>
2026-01-08 17:12:18 +0545

    scan code

--- backend/routes/scan.js
+++ backend/routes/scan.js
@@ -1,1 +1,15 @@
 
+const express = require("express");
+const { exec } = require("child_process");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+    const { path } = req.body;
+
+    exec(`python ../scanner/duplicate_finder.py "${path}"`, (err, stdout) => {
+        if (err) return res.status(500).send(err.message);
+        res.json(JSON.parse(stdout));
+    });
+});
+
+module.exports = router;
